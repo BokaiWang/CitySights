ce04bf0e19570f2f9a99fc733d53b661cad29641
BokaiWang <bk.wang1989@gmail.com>
2021-12-19 21:10:22 -0500

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Bo-Kai Wang on 2021-12-19.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CitySights
+//
+//  Created by Bo-Kai Wang on 2021-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
