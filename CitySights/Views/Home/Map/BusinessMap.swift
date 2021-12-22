aec946b05a40145adebc74a04d630b2c2e20d110
BokaiWang <bk.wang1989@gmail.com>
2021-12-22 18:08:32 -0500

    - Add BusinessMap - Add annotations to BusinessMap - Modify HomeView to toggle to map

---
+++ CitySights/Views/Home/Map/BusinessMap.swift
@@ -0,0 +1,47 @@
+//
+//  BusinessMap.swift
+//  CitySights
+//
+//  Created by Bo-Kai Wang on 2021-12-22.
+//
+
+import SwiftUI
+import MapKit
+
+struct BusinessMap: UIViewRepresentable {
+    @EnvironmentObject var model: ContentModel
+    var locations: [MKPointAnnotation] {
+        var annotations = [MKPointAnnotation]()
+        for business in model.restaurants + model.sights {
+            // If the business has a lat/long, create an MKPointAnnotation for it
+            if let lat = business.coordinates?.latitude, let long = business.coordinates?.longitude {
+                // Create a new annotation
+                let a = MKPointAnnotation()
+                a.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                a.title = business.name ?? ""
+                annotations.append(a)
+            }
+        }
+        return annotations
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        // Make the user show up on the map
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .followWithHeading
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        // Remove all annotations
+        uiView.removeAnnotations(uiView.annotations)
+        // Add the ones based on the business
+        uiView.showAnnotations(self.locations, animated: true)
+    }
+    
+    static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
+        uiView.removeAnnotations(uiView.annotations)
+    }
+}
